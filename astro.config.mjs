393d727648e61bd30beb4ed18b8b21da4fed0723
ArthurFranceschini <arthurfranvalerio@gmail.com>
2026-01-28 15:19:59 -0300

    ready to deploy + sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://miramarketing.com',
+  integrations: [sitemap({
+    i18n: {
+      defaultLocale: 'pt',
+      locales: {
+        pt: 'pt-BR',
+        es: 'es-ES'
+      }
+    }
+  })],
   vite: {
     plugins: [tailwindcss()]
   }
